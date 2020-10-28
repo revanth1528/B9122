268ee7130b66678f333e020465e17a2389a4daef
Revanth <revanth@pocketpills.com>
2020-10-28 19:51:18 +0530

    test

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-import pyttsx3
-import PyPDF2
-book = open('python_tutorial.pdf', 'rb')
-speaker = pyttsx3.init()
-pdfReader = PyPDF2.PdfFileReader(book)
-page = pdfReader.getPage(14)
-text = page.extractText()
-speaker.say(text)
-speaker.runAndWait()
+print("hello world")
